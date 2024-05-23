bd173bc9b52165ffe40b596a78a0de372162a2a4
Thomas Metten <tom.metten@acagroup.be>
2024-05-24 01:44:06 +0200

    fix(netlify): :bug: move hidden netlify form to static HTML page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Forms from "@/components/Forms/Forms";
 
 export const metadata: Metadata = {
   title: "Talentenraad",
@@ -14,9 +13,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-white">
+      <body className="bg-white antialiased">
         {children}
-        <Forms/>
       </body>
     </html>
   );
