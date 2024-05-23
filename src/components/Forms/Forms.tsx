2ff5d5bbd17292d8bc81d5f7b95ceecb6dede155
Thomas Metten <tom.metten@acagroup.be>
2024-05-24 01:34:29 +0200

    fix(netlify): :adhesive_bandage: revert netlify attribute to data-netlify attribute

--- src/components/Forms/Forms.tsx
+++ src/components/Forms/Forms.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Forms() {
     return (
         <>
-            <form className='hidden' name="feedback" netlify netlify-honeypot="bot-field" hidden>
+            <form className='hidden' name="feedback" data-netlify="true" netlify-honeypot="bot-field" hidden>
                 <textarea name="feedback"></textarea>
                 <input name="email"></input>
             </form>
