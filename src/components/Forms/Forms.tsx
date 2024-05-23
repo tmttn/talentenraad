474b6aeddba46eab6479e6e2aaaabefd621a5f06
Thomas Metten <tom.metten@acagroup.be>
2024-05-24 01:17:18 +0200

    fix(netlify): :fire: remove unwanted hidden form-name input

--- src/components/Forms/Forms.tsx
+++ src/components/Forms/Forms.tsx
@@ -4,7 +4,6 @@ function Forms() {
     return (
         <>
             <form className='hidden' name="feedback" data-netlify="true">
-                <input type="hidden" name="form-name" value="feedback" />
                 <textarea name="feedback"></textarea>
                 <input name="email"></input>
             </form>
