e2c97801f6cdd50bb81903cb980600afa8ef4343
Thomas Metten <tom.metten@acagroup.be>
2024-05-24 02:34:41 +0200

    fix(netlify): :adhesive_bandage: use the correct form name

--- src/components/FeedbackForm/FeedbackForm.tsx
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -15,7 +15,7 @@ function FeedbackForm() {
         fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contact", ...state })
+            body: encode({ "form-name": "feedback", ...state })
           })
             .then(() => alert("Success!"))
             .catch(error => alert(error));
