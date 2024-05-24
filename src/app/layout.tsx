d84099bb31b5df3c8547121fc57ab9a74e222ffb
Thomas Metten <tom.metten@acagroup.be>
2024-05-24 10:17:55 +0200

    build(vercel): :chart_with_upwards_trend: add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className="bg-white antialiased">
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
