22d1a4222ec537aed5c30be4b3bd5e6bc18d235f
Thomas Metten <tom.metten@acagroup.be>
2024-05-25 14:17:37 +0200

    fix: :pencil2: fix typo and path

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
-
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       fontFamily: {
