ac172c52def420c61bdcb6ba995a98aab2fce702
Thomas Metten <tom.metten@acagroup.be>
2024-05-25 13:25:27 +0200

    refactor: :bricks: change the feedback form to a Formspree form

--- src/components/FeedbackForm/FeedbackForm.tsx
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -1,73 +1,31 @@
+import { useForm } from '@formspree/react';
 import React from 'react'
 
-function encode(data: { [key: string]: string | null }) {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key] ?? ''))
-    .join('&')
-}
-
 export default function FeedbackForm() {
-  const [state, setState] = React.useState({})
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setState({ ...state, [e.target.name]: e.target.value })
-  }
-
-  const handleTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setState({ ...state, [e.target.name]: e.target.value })
-  }
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const form = e.currentTarget
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...state,
-      }),
-    })
-      .then(() => alert("Thanks for your feedback!"))
-      .catch((error) => alert(error))
+  const [state, handleSubmit, reset] = useForm('feedback');
+  if (state.succeeded) {
+    return <div>Thank you for signing up!</div>;
   }
 
   return (
-    <>
-      <h1>Contact</h1>
-      <form
-        name="feedback"
-        method="post"
-        action="/thanks/"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onSubmit={handleSubmit}
-      >
-        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-        <input type="hidden" name="form-name" value="feedback" />
-        <p hidden>
-          <label>
-            Don’t fill this out: <input name="bot-field" onChange={handleInputChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your email:
-            <br />
-            <input type="email" name="email" onChange={handleInputChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message:
-            <br />
-            <textarea name="message" onChange={handleTextAreaChange} />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </>
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label>
+          Je e-mailadres:
+          <br />
+          <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Je bericht:
+          <br />
+          <textarea name="message" />
+        </label>
+      </p>
+      <p>
+        <button type="submit" disabled={state.submitting}>Versturen</button>
+      </p>
+    </form>
   )
 }
