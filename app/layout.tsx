791061967925e9439f1521bf63a3b50167089a31
Thomas Metten <thomas.metten@gmail.com>
2024-06-08 12:56:16 +0200

    ci(vercel): :construction_worker: add Meticulous.ai visual test integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="nl" className="overflow-auto">
+      <head>
+        {(process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "preview") && (
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <script
+            data-project-id="cXhuDUHiYP0QQFVsZtiEudYQMB95amgg0c9tgiNr"
+            data-is-production-environment="false"
+            src="https://snippet.meticulous.ai/v1/meticulous.js"
+          />
+        )}
+      </head>
       <body className="w-screen overflow-hidden antialiased bg-gradient-to-b from-white to-base-100">
         {children}
         <SpeedInsights />
