a191adb17f3f272df0027f55a09968e90fce09e5
Thomas Metten <thomas.metten@gmail.com>
2024-06-08 13:50:40 +0200

    refactor: :coffin: remove optional indicator
    The navigation model consist completely out of required fields.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 type HeaderProps = {
-    navigation: { value: { data: { links?: { url: string, text: string }[] } } };
+    navigation: { value: { data: { links: { url: string, text: string }[] } } };
 };
 
 const Header: React.FC<Readonly<HeaderProps>> = ({ navigation }) => {
@@ -13,7 +13,7 @@ const Header: React.FC<Readonly<HeaderProps>> = ({ navigation }) => {
             <div className="py-10 header text-base-content max-w-[1280px] flex w-full items-center gap-10">
                 <Image priority width={1600} height={890} src="/Logo.jpeg" alt="Logo" className="w-[300px] h-auto" />
                 <nav className="flex flex-row gap-20 text-lg font-bold">
-                    {links?.map((item) => (
+                    {links.map((item) => (
                         <a key={item.url} href={item.url} className="link link-hover">{item.text}</a>
                     ))}
                 </nav>
