d2ad87507b31163a2a8062a0b1c7ad4437da741b
Thomas Metten <thomas.metten@gmail.com>
2025-02-24 13:03:57 +0100

    fix: fix vercel build issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,4 @@
 import type {Config} from 'tailwindcss';
-import daisyui from 'daisyui';
-import light from 'daisyui/src/theming/themes';
 
 const config: Config = {
 	content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
@@ -12,25 +10,7 @@ const config: Config = {
 		},
 	},
 	plugins: [
-		daisyui,
+		require('daisyui'),
 	],
-	daisyui: {
-		themes: [
-			{
-				light: {
-					light,
-					primary: '#ea247b',
-					secondary: '#afbd43',
-					accent: '#f1b357',
-					neutral: '#57534e',
-					'base-100': '#F7F5DD',
-					info: '#0ea5e9',
-					success: '#4ade80',
-					warning: '#fdba74',
-					error: '#f87171',
-				},
-			},
-		],
-	},
 };
 export default config;
